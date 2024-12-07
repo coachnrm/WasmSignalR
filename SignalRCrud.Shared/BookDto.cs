e1586b303e09c2ffcb67730dc7b6fec3b057e71e
home <nuttapong_coach@hotmail.com>
2024-12-07 12:52:45 +0700

    add code

--- SignalRCrud.Shared/BookDto.cs
+++
@@ -1,9 +0,0 @@
-namespace SignalRCrud.Shared;
-
-public class BookDto
-{
-    public string Isbn { get; set; }
-    public string Name { get; set; }
-    public string Author { get; set; }
-    public double Price { get; set; }
-}
